7d60b45a2fc9ba568b90688a9ae4ddb644bdaae4
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-24 11:25:10 -0400

    Enh: Url to config

--- Module.php
+++ Module.php
@@ -2,9 +2,17 @@
 
 namespace humhub\modules\gifs;
 
+use yii\helpers\Url;
 use humhub\modules\content\components\ContentContainerModule;
 
 class Module extends ContentContainerModule
 {
 
+    /**
+     * @inheritdoc
+     */
+    public function getConfigUrl()
+    {
+        return Url::to(['/gifs/admin/index']);
+    }
 }
