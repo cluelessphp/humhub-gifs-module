31cd760f77e850002487d8a1bc2fdc6ee3c51adf
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-24 11:27:18 -0400

    Enh: Remove Admin Panel option + Cleanup

--- config.php
+++ config.php
@@ -4,19 +4,14 @@ namespace humhub\modules\gifs;
 
 use humhub\modules\gifs\Events;
 use humhub\modules\gifs\Module;
-use humhub\modules\admin\widgets\AdminMenu;
 use humhub\modules\content\widgets\WallEntryLinks;
-
 
 return [
     'id' => 'gifs',
     'class' => Module::class,
     'namespace' => 'humhub\modules\gifs',
     'events' => [
-       
-        ['class' => AdminMenu::class, 'event' => AdminMenu::EVENT_INIT, 'callback' => [Events::class, 'onAdminMenuInit']],
-		['class' => WallEntryLinks::class, 'event' => WallEntryLinks::EVENT_INIT, 'callback' => [Events::class, 'onWallEntryLinksInit']]
-		
+	['class' => WallEntryLinks::class, 'event' => WallEntryLinks::EVENT_INIT, 'callback' => [Events::class, 'onWallEntryLinksInit']]
     ]
 ];
 ?>
