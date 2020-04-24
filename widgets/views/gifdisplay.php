39432f419f2790de03c72d4d2fc0bc506051b4a4
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-24 12:00:49 -0400

    Enh: Minor cleanup

--- widgets/views/gifdisplay.php
+++ widgets/views/gifdisplay.php
@@ -1,16 +1,18 @@
 <?php
-/* @var $this humhub\components\View */
+
+/* @var $this View */
 
 use yii\helpers\Url;
+use humhub\modules\ui\view\components\View;
+
 ?>
 
 <?php if (!Yii::$app->user->isGuest): ?>
     <span class="gifLinkContainer" id="gifLinkContainer_<?= $id ?>">
-        <a href="<?=Url::to(['/gifs/share', 'id' => $id]); ?>" data-target="#globalModal" class="showComments">
-            GIFS
-        </a>
+        <a href="<?= Url::to(['/gifs/share', 'id' => $id]); ?>" data-target="#globalModal" class="showComments">GIFs </a>
     </span>
 <?php endif; ?>
+
 <script>
 $(document).on("click",'.showComments',function(e) {
 	console.log('got here');
@@ -19,4 +21,3 @@ $(document).on("click",'.showComments',function(e) {
     currentCommentContainer = $(this).closest('.media').find('.comment-container').slideDown();	
 });
 </script>
-
